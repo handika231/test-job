e48f1027bc29e26c610043f4687b16cb7ee73265
Handika Rafli A <shandika231@gmail.com>
2022-10-28 16:12:55 +0700

    add responsive framework

--- lib/presentation/application.dart
+++ lib/presentation/application.dart
@@ -1,16 +1,31 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:money_app/presentation/pages/home_page.dart';
+import 'package:responsive_framework/responsive_framework.dart';
 
 class Application extends StatelessWidget {
   const Application({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const GetMaterialApp(
+    return GetMaterialApp(
+      builder: (context, child) => ResponsiveWrapper.builder(
+        child,
+        maxWidth: 1200,
+        minWidth: 450,
+        defaultScale: true,
+        breakpoints: [
+          const ResponsiveBreakpoint.resize(450, name: MOBILE),
+          const ResponsiveBreakpoint.autoScale(800, name: MOBILE),
+          const ResponsiveBreakpoint.autoScale(800, name: TABLET),
+          const ResponsiveBreakpoint.autoScale(1000, name: TABLET),
+          const ResponsiveBreakpoint.resize(1200, name: DESKTOP),
+          const ResponsiveBreakpoint.autoScale(2460, name: "4K"),
+        ],
+      ),
       debugShowCheckedModeBanner: false,
       title: 'Money App',
-      home: HomePage(),
+      home: const HomePage(),
     );
   }
 }
